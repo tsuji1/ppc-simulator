0dad64993b87629ac9cdf416d9aa24e8291227e9
Yuzuki Tsuji <tsuji@hpc.is.uec.ac.jp>
2024-07-25 02:06:57 +0900

    chore: Update exclusivenbits.py to modify Refbits in MultiLayerCacheExclusive configuration

---
+++ scripts/exclusivenbits.py
@@ -0,0 +1,43 @@
+import os
+import subprocess
+
+
+import json
+
+# JSONファイルのパス
+src_file_path = '../simulator-settings/MultiLayerCacheExclusive.json'
+
+# JSONファイルを読み込む
+with open(src_file_path, 'r') as file:
+    data:dict = json.load(file)
+
+result_data = {}
+dst_file_path = '../result/10-24bits_exclusive.json'
+with open(dst_file_path, 'w') as file:
+    json.dump({}, file, indent=4)
+
+data["Cache"]["Rule"] = "../rules/wide.rib.20240625.1400.rule"  
+# Refbitsを変更する
+for refbits in range(10, 25):
+    tmp_file_path = '../simulator-settings/tmp.json'
+    
+    for layer in data["Cache"]["CacheLayers"]:
+        if layer["Type"] == "FullAssociativeDstipNbitLRUCache":
+            if layer["Size"] == 256:
+                layer["Refbits"] = refbits  # 変更する値
+
+
+    # 更新されたJSONデータをファイルに書き込む
+    with open(tmp_file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+    
+    # シミュレータを実行する
+    cmd = f'../main ../simulator-settings/tmp.json ../parsed-pcap/202406251400.p7'
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    _json_data = json.loads(result.stdout)
+    result_data[refbits] = _json_data
+    print(f"Refbits: {refbits} Done")
+else:
+    with open(dst_file_path, 'w') as file:
+        json.dump(result_data, file, indent=4)
+        print("Done")
