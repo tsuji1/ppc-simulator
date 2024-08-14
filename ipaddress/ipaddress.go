e3941c698fbcaf803a4aa0ddb6195b46472a2f49
Yuzuki Tsuji <tsuji@hpc.is.uec.ac.jp>
2024-08-14 18:04:32 +0900

    refactor: Improve IPaddress struct and methods

--- ipaddress/ipaddress.go
+++ ipaddress/ipaddress.go
@@ -1,51 +1,59 @@
 package ipaddress
 
 import (
-	"fmt"
-	"strconv"
-	"net"
-	"strings"
-	"regexp"
-	"math"
-	"github.com/praserx/ipconv"
 	"encoding/binary"
+	"fmt"
+	"math"
+	"net"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/praserx/ipconv"
 )
 
-type IPaddress struct{
-	ipaddress	uint32
+// IPaddress型の定義。内部で32ビットのIPアドレスを保持。
+type IPaddress struct {
+	ipaddress uint32
 }
 
-func (hoge IPaddress) Uint32() uint32{
+// Uint32メソッドは、IPaddress構造体のipaddressフィールドを返す。
+func (hoge IPaddress) Uint32() uint32 {
 	return hoge.ipaddress
 }
 
-func (hoge IPaddress) BitString() string{
+// BitStringメソッドは、32ビットのビット列としてIPアドレスを返す。
+func (hoge IPaddress) BitString() string {
 	return hoge.MaskedBitString(32)
 }
 
-func (hoge IPaddress) MaskedBitString(prefix int) string{
+// MaskedBitStringメソッドは、指定されたプレフィックス長に基づいてビット列を返す。
+/*
+	input:  ipアドレス(整数)とマスク
+	output: ビット列 (マスク分欠け)
+	ex. 255, 3 = 0.0.0.255/29
+		00000000 00000000 00000000 11111
+*/
+func (hoge IPaddress) MaskedBitString(prefix int) string {
 	if 0 <= prefix && prefix <= 32 {
 		str := fmt.Sprintf("%032s", strconv.FormatInt(int64(hoge.ipaddress), 2))
 		return str[:prefix]
-	}else{
+	} else {
 		panic(fmt.Sprintf("MaskedBitString:mask or prefix is %d", prefix))
 	}
-	/*
-	input:	ipアドレス(整数)とマスク
-	output:	ビット列 (マスク分欠け)
-	ex. 255, 3 = 0.0.0.255/29
-		00000000 00000000 00000000 11111
-	*/
+
 }
 
-func (hoge IPaddress) String() string{
+// Stringメソッドは、IPアドレスをドット区切りの文字列として返す。
+func (hoge IPaddress) String() string {
 	ip := make(net.IP, 4)
 	binary.BigEndian.PutUint32(ip, hoge.ipaddress)
 	return ip.String()
 }
 
+// SetIPメソッドは、様々な入力形式からIPアドレスを設定する。
 func (hoge *IPaddress) SetIP(input interface{}) {
-	switch a := input.(type){
+	switch a := input.(type) {
 	case int:
 		hoge.ipaddress = uint32(a)
 	case string:
@@ -58,53 +66,62 @@ func (hoge *IPaddress) SetIP(input interface{}) {
 	}
 }
 
+// regIPは、ビット文字列をチェックするための正規表現。
 var regIP = regexp.MustCompilePOSIX(`[^01]+`)
 
-func isBitString(a string) bool{
-	if !(regIP.MatchString(a)) && len(a)<=32{
+// isBitString関数は、入力がビット文字列であるかを判定する。
+func isBitString(a string) bool {
+	if !(regIP.MatchString(a)) && len(a) <= 32 {
 		return true
 	}
 	return false
 }
 
+// Reverse関数は、文字列を逆順にする。
 func Reverse(s string) string {
 	runes := []rune(s)
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]}
+		runes[i], runes[j] = runes[j], runes[i]
+	}
 	return string(runes)
 }
 
-func bitStringToIP(a string) string{
+// bitStringToIP関数は、ビット文字列をドット区切りのIPアドレス文字列に変換する。
+func bitStringToIP(a string) string {
 	b := a + "00000000000000000000000000000000"
 	b = b[0:32]
-	c := func(c string) int{
+	c := func(c string) int {
 		ans := 0
-		for i,item := range(Reverse(c)){
-			if item == 49{
-				ans += int(math.Pow(2,float64(i)))}}
+		for i, item := range Reverse(c) {
+			if item == 49 {
+				ans += int(math.Pow(2, float64(i)))
+			}
+		}
 		return ans
 	}
-	return fmt.Sprintf("%d.%d.%d.%d",c(b[0:8]), c(b[8:16]), c(b[16:24]), c(b[24:32]))
+	return fmt.Sprintf("%d.%d.%d.%d", c(b[0:8]), c(b[8:16]), c(b[16:24]), c(b[24:32]))
 }
 
+// NewIPaddress関数は、入力をもとに新しいIPaddress構造体を作成する。
 func NewIPaddress(input interface{}) IPaddress {
 	var temp uint32
-	switch a := input.(type){
-		case int:
-			temp = uint32(a)
-		case string:
-			if strings.Contains(a, "."){
-				i, _ := ipconv.IPv4ToInt(net.ParseIP(a))
-				temp = i
-			}else if isBitString(a){
-				i, _ := ipconv.IPv4ToInt(net.ParseIP(bitStringToIP(a)))
-				temp = i
-			}else{
-				panic(fmt.Sprintf("NewIPaddress:%v is not IPaddress string", input))}
-		case uint32:
-			temp = a
-		default:
-			panic(fmt.Sprintf("NewIPaddress:%v (type:%T) is not IPaddress", input, a))
+	switch a := input.(type) {
+	case int:
+		temp = uint32(a)
+	case string:
+		if strings.Contains(a, ".") {
+			i, _ := ipconv.IPv4ToInt(net.ParseIP(a))
+			temp = i
+		} else if isBitString(a) {
+			i, _ := ipconv.IPv4ToInt(net.ParseIP(bitStringToIP(a)))
+			temp = i
+		} else {
+			panic(fmt.Sprintf("NewIPaddress:%v is not IPaddress string", input))
+		}
+	case uint32:
+		temp = a
+	default:
+		panic(fmt.Sprintf("NewIPaddress:%v (type:%T) is not IPaddress", input, a))
 	}
 	return IPaddress{
 		ipaddress: temp,
