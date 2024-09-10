28be984673317a59a7c310166def019a3986c146
= <=>
2024-09-11 02:40:29 +0900

    chore: Update capacity settings for exclusive lbits simulation

--- scripts/exclusive_lbits_change_capacity_result.py
+++ scripts/exclusive_lbits_change_capacity_result.py
@@ -1,7 +1,7 @@
 import os
 import json
 from models.MultiLayerCacheExclusive import MultiLayerCacheExclusive,AnalysisResults
-
+import copy
 # refbitsごとにまとめよう
 first = 1
 last = 25
@@ -10,7 +10,9 @@ cap_first = 64 * 2
 cap_last = int(4096 /2)
 interval =64 * 2 
 capacity = [i for i in range(cap_first,cap_last+1,interval)] # 64から4096
-
+capacity_nbits = copy.copy(capacity)
+capacity_32bits = copy.copy(capacity)
+capacity_32bits.append(1)
 
 def aggregate_result(refbits,cache_32bit_capacity, cache_nbit_capacity):
     result_data = {}
