2354518c688bbd6cc5f223326b0dab0bdd960988
tsuji <tsuji@hpc.is.uec.ac.jp>
2024-10-22 03:31:16 +0900

    change name

--- main.go
+++ main.go
@@ -694,8 +694,8 @@ func main() {
 	} else {
 
 		capacity := make([]int, 0, 30)
-		for i := 2; i <= 5; i++ {
-			capacity = append(capacity, 1<<uint(i*2))
+		for i := 2; i <= 20; i++ {
+			capacity = append(capacity, 1<<uint(i))
 		}
 
 		// print capacity
